b8a806288b1987cfbe9ba2530572150d274a28ce
hnaing <hnin.naing@yahoo.com>
2014-12-24 17:37:09 -0800

    working draft

--- articleScraper.java
+++ articleScraper.java
@@ -1,22 +1,29 @@
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.FileWriter;
-
+import java.io.InputStreamReader;
 import java.util.List;
 
 import com.fiskkit.Fetcher;
+import com.opencsv.CSVWriter;
 
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
 //import opennlp.tools.util.InvalidFormatException;
 
+/** The articleScraper project
+ * @author Hnin Naing
+ * */
+
 public class articleScraper
 {
-    public static void main(String[] args) throws FileNotFoundException
+	/**Extract sentences from live articles.
+	 */	
+    public static void main(String[] args) throws IOException
     {
-    	//put in training data
+    	//train data using opennlp api
     	InputStream modelIn = new FileInputStream("en-sent.bin");
     	SentenceModel model = null;
     	try {
@@ -35,14 +42,33 @@ public class articleScraper
     	  }
     	}
     	SentenceDetectorME sentenceDetector = new SentenceDetectorME(model);
+
+    	//Get article address and output file name as input
+    	System.out.print("Article address: ");
+    	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    	String web = input.readLine();
+    	System.out.print("Output file name: ");
+    	input = new BufferedReader(new InputStreamReader(System.in));
+    	String filename = input.readLine();
+
     	
+    	
+    	CSVWriter file = new CSVWriter(new FileWriter(filename),'\n');
     	
     	//extract the paragraphs
-    	List<String> paragraphs = Fetcher.pullAndExtract("http://www.weeklystandard.com/blogs/intel-chief-blasts-obama_802242.html");
-    	String[] sentences = sentenceDetector.sentDetect(paragraphs.get(1));
+    	List<String> paragraphs = Fetcher.pullAndExtract(web);
     	
-    	
-//        System.out.println(test.get(0).charAt(1));
-    	System.out.println(sentences[0]);
+//    	file.writeNext(nextLine);(paragraphs); 
+    	for (int i = 0; i < paragraphs.size(); i += 1)
+    	{
+    		String[] sentences = sentenceDetector.sentDetect(paragraphs.get(i));
+    		
+    		file.writeNext(sentences);
+    		
+    	}
+    	file.close();
+
+    	System.out.println("Done");
     }
+    
 }
